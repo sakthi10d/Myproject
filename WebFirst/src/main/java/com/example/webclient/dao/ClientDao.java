cecf0ea1d0ded3d9f45b05fd1f224f4c2401c2d3
sakthi10d <sakthi.g@10decoders.in>
2022-03-31 10:36:13 +0530

    CLIENTDEMO!

---
+++ WebFirst/src/main/java/com/example/webclient/dao/ClientDao.java
@@ -0,0 +1,27 @@
+package com.example.webclient.dao;
+
+public class ClientDao {
+    
+	private int studentId;
+	private String studentName;
+	private String address;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
